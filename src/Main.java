f6b4a76bb68c32e104474f6fcbdd76a64905ea0a
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-20 20:24:29 +0300

    Задача №2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
-public class Main {
-            public static void main(String[] args) {
-            task1();
-        }
+import java.util.Locale;
 
-        public static void task1() {
-            System.out.println("Задача 1");
-            String firstName = "Ivan";
-            String middleName = "Ivanovich";
-            String lastName = " Ivanov";
-            String fullName = lastName + ' ' + firstName + ' ' + middleName;
-            System.out.println("ФИО сотрудника — " + fullName);
-        }
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println(" Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+    }
+}
